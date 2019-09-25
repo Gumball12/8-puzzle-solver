5c8692dbf6fe66283dc74eb71466c2a6bce7f792
HaeJun Seo <kan02134@gmail.com>
2019-09-25 22:36:49 +0900

    Feat: Create DFS and BFS solver

--- index.py
+++ index.py
@@ -3,8 +3,9 @@
 # @author shj <https://github.com/Gumball12/8-puzzle-solver>
 """
 
-from random import shuffle
+from random import randint
 from copy import deepcopy
+from collections import deque
 
 """ tools """
 
@@ -15,11 +16,6 @@ def getUserInput():
 # generate an array
 def getArray(len):
   a = list(range(len))
-  return a
-
-# shuffle array
-def shuffleArray(a):
-  shuffle(a)
   return a
 
 # chunk array by n
@@ -52,13 +48,13 @@ def testing():
     # check movable
     if (puzzle.isMovable()[d]):
       # move to direction
-      puzzle.move(d)
+      puzzle = puzzle.move(d) # update puzzle
 
       # check updated
       if (str(puzzle) == str(tempPuzzle)): # error
         print('failed to', directionNames[d], 'direction test: puzzle instance did not updated')
         return False # failure
-      else:
+      else: # success
         print('success', directionNames[d], 'direction test')
         tempPuzzle = deepcopy(puzzle) # update tempPuzzle
     else:
@@ -66,15 +62,27 @@ def testing():
 
   return True # success
 
+# append to array with multiple arguments
+def append(arr, *items):
+  for i in items:
+    arr.append(i)
+
+  return arr
+
 """ process """
 
 # Puzzle class
 class Puzzle:
   # constructor
-  def __init__(self, size = 3): # default size is 3
+  def __init__(self, matrix = None, size = 3): # default size is 3
     # init matrix
-    self.matrix = arrayToMatrix(shuffleArray(getArray(size * size)), size)
-    self.size = size
+    if (matrix):
+      self.matrix = matrix
+      self.size = len(matrix)
+    else:
+      self.matrix = arrayToMatrix(getArray(size * size), size)
+      self.size = size
+      self.shuffle()
 
   # move operator
   # @param {number} op operators (0: left, 1: top, 2: right, 3: bottom)
@@ -102,10 +110,11 @@ class Puzzle:
     else: # wroing direction
       return False
 
-    self.matrix = arrayToMatrix(swap(arr, ind, swapInd), self.size)
+    # unmutable method
+    # self.matrix = arrayToMatrix(swap(arr, ind, swapInd), self.size)
 
-    # return new matrix
-    return self.matrix
+    # return new Puzzle
+    return Puzzle(arrayToMatrix(swap(arr, ind, swapInd), self.size))
 
   # check target is movable
   def isMovable(self):
@@ -123,12 +132,30 @@ class Puzzle:
     # return status
     return (left, top, right, bottom)
 
+  # shuffle this matrix
+  def shuffle(self):
+    for i in range(randint(50, 100)):
+      direction = randint(0, 3)
+      if (self.isMovable()[direction]):
+        self.matrix = self.move(direction).matrix
+
   # toString
   def __str__(self):
     return ', '.join(map(str, self.matrix)) # map: string guard
 
 # process
 def process():
+  # create puzzle instances
+  size = 4
+
+  puzzle = Puzzle(None, size)
+  # puzzle = Puzzle([[1, 7, 4], [8, 0, 2], [3, 6, 5]])
+  # puzzle = Puzzle([[1, 2, 5], [3, 4, 8], [0, 6, 7]])
+  goal = Puzzle(arrayToMatrix(getArray(size * size), size)) # goal state
+
+  print('Create Puzzle instance', puzzle)
+  print('Create goal state', goal)
+
   # get user input
   inpt = getUserInput()
 
@@ -136,13 +163,44 @@ def process():
     print("Wrong type: " + inpt)
     inpt = getUserInput()
 
-  # create puzzle instance
-  puzzle = Puzzle()
+  # define arrays using deque
+  opens = deque([puzzle])
+  closes = deque([])
+
+  # searching
+  while len(opens) != 0:
+    # get leftmost element
+    x = opens.popleft()
+
+    if (str(x) == str(goal)): # when success
+      print('length of closed:', len(closes))
+      return True
+    else: # before success
+      # put x into the closes array
+      closes.append(str(x))
+
+      # append(opens, *[x.move(d) for d in range(0, 4) if x.isMovable()[d] and str(x.move(d)) not in closes])
+
+      movable = x.isMovable()
+
+      # generate x's children
+      for d in range(0, 4):
+        if (movable[d]):
+          moved = x.move(d)
+
+          if (str(moved) not in closes):
+            if (inpt == 1): # DFS
+              opens.appendleft(moved)
+            else: # BFS
+              opens.append(moved)
+
+  return False
 
 # init
 if __name__ == "__main__":
   if (testing()):
     print('==========\nSuccess unit testing\n==========\n')
-    process()
+    result = process()
+    print("solved" if result else "failed")
   else:
     print('==========\nFailed to unit testing\n==========\n')
